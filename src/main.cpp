1908d7fa3952cb935cd6ef4e336c88be643f7057
Mykal Sullivan <mykalsullivan@outlook.com>
2024-06-02 21:30:17 -0500

    Made perspective projection. For some reason, the pyramid starts to spin *extremely* fast and I'm not sure why

--- src/main.cpp
+++ src/main.cpp
@@ -20,8 +20,11 @@ const unsigned int WIDTH = 640, HEIGHT = 480;
 // IDs for VAO, VBO, and shader objects
 unsigned int vao, vbo, shader;
 
+// Index buffer
+unsigned int ibo;
+
 // Uniform variables
-unsigned int uniformModel;
+unsigned int uniformModel, uniformProjection;
 bool direction = true;
 float triOffset = 0.0f, triMaxOffset = 1.0f, triTranslationIncrement = 0.015f;
 
@@ -34,12 +37,13 @@ static const char* vertexShader =
         "\n"
         "layout (location = 0) in vec3 pos;\n"                  /* Note: 'pos' is a built-in variable in OpenGL */
         "uniform mat4 model;\n"
+        "uniform mat4 projection;\n"
         "\n"
         "out vec4 vertexColor;\n"
         "\n"
         "void main()\n"
         "{\n"
-        "   gl_Position = model * vec4(pos.x, pos.y, pos.z, 1.0);\n"    /* Note: 'gl_Position' is also built-in */
+        "   gl_Position = projection * model * vec4(pos.x, pos.y, pos.z, 1.0);\n" /* Note: 'gl_Position' is also built-in */
         "   vertexColor = vec4(clamp(pos, 0.0f, 1.0f), 1.0f);\n"        /* Note: 'clamp()' keeps values in a specified range */
         "}\n";
 
@@ -58,16 +62,29 @@ static const char* fragmentShader =
 
 void createTriangle()
 {
+    unsigned int indices[] = {
+            0, 3, 1,
+            1, 3, 2,
+            2, 3, 0,
+            0, 1, 2
+    };
+
     // Create vertex coordinates
     float vertices[] = {
             -1.0f, -1.0f, 0.0f,
-            0.0f, 1.0f, 0.0f,
-            1.0f, -1.0f, 0.0f
+            0.0f, -1.0f, 1.0f,
+            1.0f, -1.0f, 0.0f,
+            1.0f, 1.0f, 0.0f
     };
 
     // Generate and bind VAO
     glGenVertexArrays(1, &vao);
     glBindVertexArray(vao);
+
+    // Generate, bind, and buffer index array
+    glGenBuffers(1, &ibo);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
     // Generate, bind, and buffer VBO
     glGenBuffers(1, &vbo);
@@ -85,6 +102,7 @@ void createTriangle()
     glEnableVertexAttribArray(0);
 
     glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 
     glBindVertexArray(0);
 }
@@ -162,6 +180,7 @@ void compileShaders()
     }
 
     uniformModel = glGetUniformLocation(shader, "model");
+    uniformProjection = glGetUniformLocation(shader, "projection");
 }
 
 int main()
@@ -215,12 +234,21 @@ int main()
         return 1;
     }
 
+    // Used to prevent sides from being rendered incorrectly
+    glEnable(GL_DEPTH_TEST);
+
     // Setup viewport size
     glViewport(0, 0, bufferWidth, bufferHeight);
 
     // Create triangle and compile shaders
     createTriangle();
     compileShaders();
+
+    glm::mat4 model(1.0f);
+    glm::mat4 projection = glm::perspective(45.0f, (float) bufferWidth/(float) bufferHeight, 0.1f, 100.0f);
+
+    // Move the pyramid away from the user
+    model = glm::translate(model, glm::vec3(0.0f, 0.0f, -5.0f));
 
     // Main loop
     while (!glfwWindowShouldClose(mainWindow))
@@ -245,7 +273,6 @@ int main()
             auto g = (float) std::abs(std::sin(i));
             auto b = (float) std::abs(std::sin(i-(2*M_PI/3)));
 
-            glm::mat4 model(1.0f);
             if (direction)
             {
                 triOffset += triTranslationIncrement;
@@ -267,26 +294,28 @@ int main()
 
             // Clear window
             glClearColor(r, g, b, 1.0f);
-            glClear(GL_COLOR_BUFFER_BIT);
+            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
             glUseProgram(shader);
 
-            model = glm::rotate(model, currentAngle * (float) M_PI/180, glm::vec3(1.0f, 1.0f, 0.5f));
-            //model = glm::translate(model, glm::vec3(triOffset, triOffset, 0.0f));
-            model = glm::scale(model, glm::vec3(0.5f, 0.5f, 1.0f));
+            model = glm::rotate(model, currentAngle * (float) M_PI/180, glm::vec3(0.1f, 0.2f, 0.3f));
+            //model = glm::translate(model, glm::vec3(1.0f, 1.0f, 1.0f));
+            //model = glm::scale(model, glm::vec3(1.0f + i, 1.0f + i, 1.0f));
 
-            glUniform1f((int) uniformModel, triOffset);
             glUniformMatrix4fv((int) uniformModel, 1, false, glm::value_ptr(model));
+            glUniformMatrix4fv((int) uniformProjection, 1, false, glm::value_ptr(projection));
 
             glBindVertexArray(vao);
-            glDrawArrays(GL_TRIANGLES, 0, 3);
+            glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+            glDrawElements(GL_TRIANGLES, 12, GL_UNSIGNED_INT, nullptr);
+            glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
             glBindVertexArray(0);
 
             glUseProgram(0);
 
             std::this_thread::sleep_for(std::chrono::microseconds(16667));
             i += 0.005f;
-            currentAngle += 1.5f;
+            currentAngle += 0.05f;
         }
 
         // Swap display buffers
